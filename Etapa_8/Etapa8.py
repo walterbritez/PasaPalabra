f39912fda0acd4e3cdcc4b43b6b1d0eb6e12c295
Felipe Gazcon <pipegazcon123@gmail.com>
2023-06-19 00:30:50 -0300

    Terminado parte 10, correccion parte 8

--- Etapa_8/Etapa8.py
+++ Etapa_8/Etapa8.py
@@ -1,9 +1,15 @@
 import csv
 import os
 
-def formar_archivo_csv(min_largo_palabra):
+RUTA_ARCHIVO = os.path.join("Etapa_10", "configuracion.csv")
+with open(RUTA_ARCHIVO, "r", encoding="utf-8") as configuracion_juego:
+    lector = csv.reader(configuracion_juego, delimiter=",")
+    listado = list(lector)
+MIN_LARGO_PALABRA = int(listado[0][1])
+
+def formar_archivo_csv():
     '''
-    Esta función lee los archivos "palabras.txt" y "definiciones.txt" y los escribe en un archivo CSV en formato CSV.
+    Esta función lee los archivos "palabras.txt" y "definiciones.txt" y los escribe en un archivo de formato CSV.
     Hecho por: Felipe Gazcon
     Modificado por: Brian Duarte
     Corregido por: 
@@ -24,7 +30,7 @@ def formar_archivo_csv(min_largo_palabra):
         linea_definicion = definiciones.readline().strip()
 
         while linea_palabra:
-            if len(linea_palabra) >= min_largo_palabra and linea_palabra.isalpha():
+            if len(linea_palabra) >= MIN_LARGO_PALABRA and linea_palabra.isalpha():
                 escritor.writerow([linea_palabra, linea_definicion])
 
             linea_palabra = palabras.readline().strip()
@@ -32,4 +38,4 @@ def formar_archivo_csv(min_largo_palabra):
 
         print("Archivo 'nuevo_archivo.csv' generado correctamente.")
 
-formar_archivo_csv(5)
+formar_archivo_csv()
