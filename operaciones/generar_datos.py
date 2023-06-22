4be93595c782811233b328894d579a978e6c88d1
Felipe Gazcon <pipegazcon123@gmail.com>
2023-06-22 01:00:35 -0300

    Correcciones modulo .csv

--- operaciones/generar_datos.py
+++ operaciones/generar_datos.py
@@ -3,14 +3,6 @@ from operaciones.constantes_operaciones import *
 
 RUTA_ARCHIVO = os.path.join(ARCHIVO_PY_CONFIG, ARCHIVO_CSV_CONFIG)
 
-with open(RUTA_ARCHIVO, "r", encoding="utf-8") as configuracion_juego:
-    contenido = configuracion_juego.read().splitlines()
-
-listado = [linea.split(",") for linea in contenido]
-
-MIN_LARGO_PALABRA = int(listado[IND_PALABRA][IND_DEFINICION])
-
-
 def formar_archivo_csv():
     '''
     Esta función lee los archivos "palabras.txt" y "definiciones.txt" y los escribe en un archivo de formato CSV.
@@ -18,13 +10,16 @@ def formar_archivo_csv():
     Modificado por: Brian Duarte / Walter Britez
     Corregido por: 
     '''
-
+    with open(RUTA_ARCHIVO, "r", encoding="utf-8") as configuracion_juego:
+        listado = [line.split(",") for line in configuracion_juego]
+        MIN_LARGO_PALABRA = int(listado[0][1])
+        
     with open(ARCHIVO_DEFINICIONES, "r", encoding="utf-8") as definiciones, open(ARCHIVO_PALABRAS, "r", encoding="utf-8") as palabras, open(ARCHIVO_DATOS, "w", encoding="utf-8", newline="") as datos:
         linea_palabra = palabras.readline().rstrip()
         linea_definicion = definiciones.readline().rstrip()
 
         while linea_palabra:
-            if len(linea_palabra) >= MIN_LARGO_PALABRA and linea_palabra.isalpha():
+            if len(linea_palabra) > MIN_LARGO_PALABRA and linea_palabra.isalpha():
                 datos.write(f"{linea_palabra},{linea_definicion}\n")
 
             linea_palabra = palabras.readline().strip()
